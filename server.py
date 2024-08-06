76f3b04247a25cad311e68b62eaad3bc50cbc2dc
Eric De Leenheer <105471603+Edspeedy1@users.noreply.github.com>
2024-08-06 13:48:20 -0400

    adding env port

--- server.py
+++ server.py
@@ -2,30 +2,46 @@ import RangeHTTPServer
 import socketserver
 import os
 import time
+import threading
 import random
 import json
 
-PORT = 8026
+PORT = int(os.getenv('PORT', 8026))
 
 print("starting server")
+
+class ThreadedHTTPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
 
 class CustomRequestHandler(RangeHTTPServer.RangeRequestHandler):
     def __init__(self, request, client_address, server):
         super().__init__(request, client_address, server)
 
     def end_headers(self):
-        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Origin', '*')  # Add CORS header
         super().end_headers()
 
-# Create the server
-server = socketserver.TCPServer(('', PORT), CustomRequestHandler)
+    def send_file(self, status_code, content):
+        self.send_response(status_code)
+        self.send_header('Content-Type', 'text/plain')
+        self.send_header('Content-Length', str(len(content)))
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
 
-print(f"Server started on port {PORT}")
+        self.wfile.write(content.encode())
 
-# Start the server
-try:
-    server.serve_forever()
-except KeyboardInterrupt:
-    print("Shutting down server")
-    server.shutdown()
-    server.server_close()
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+
+        super().do_GET()
+
+with ThreadedHTTPServer(("", PORT), CustomRequestHandler) as httpd:
+    print("serving at port", PORT)
+    # Start a new thread for serving requests
+    for i in range(10):
+        server_thread = threading.Thread(target=httpd.serve_forever)
+        server_thread.daemon = True  # Daemonize the thread so it exits when the main thread exits
+        server_thread.start()
+        # Wait for the server thread to finish (if ever)
+        server_thread.join()
