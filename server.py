e120f2018498023fbb6a474c7ecdd40d303acd03
Eric De Leenheer <105471603+Edspeedy1@users.noreply.github.com>
2024-08-11 12:46:43 -0400

    comments

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ import json
 import sqlite3
 import hashlib
 
+# logging for debug messages on server console
 import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -16,6 +17,7 @@ HTTP_PORT = int(os.getenv('PORT', 8026))
 
 print("starting server")
 
+# class for keeping track of active sessions
 class ConnectedClient:
     def __init__(self, username, sessionID):
         self.username = username
@@ -28,6 +30,7 @@ class ConnectedClient:
     def update_last_active_time(self):
         self.lastActiveTime = time.time()
 
+# singleton database manager to help with the inactivity check thread
 class DatabaseManager:
     _instance = None
 
@@ -50,11 +53,15 @@ class DatabaseManager:
         self.login_conn.close()
         self.player_data_conn.close()
 
-
+# legacy code, was necessary but IDK anymore and i'm too scared to touch it
 class ThreadedHTTPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
 
+
+# dictionary of active sessions
 sessions = {}
+
+# the meat and potatoes of the server
 class CustomRequestHandler(RangeHTTPServer.RangeRequestHandler):
     def __init__(self, request, client_address, server):
         self.db = DatabaseManager()  # Use shared database manager
@@ -64,13 +71,12 @@ class CustomRequestHandler(RangeHTTPServer.RangeRequestHandler):
         self.send_header('Access-Control-Allow-Origin', '*')  # Add CORS header
         super().end_headers()
 
-    def send_file(self, status_code, content):
+    def send_file(self, status_code, content): # unused
         self.send_response(status_code)
         self.send_header('Content-Type', 'text/plain')
         self.send_header('Content-Length', str(len(content)))
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
-
         self.wfile.write(content.encode())
 
     def do_GET(self):
@@ -97,8 +103,10 @@ class CustomRequestHandler(RangeHTTPServer.RangeRequestHandler):
                 logger.error(e)
                 logger.info(data["session"])
                 self.send_json_response(200, {'error': 'Invalid session'})
+
         elif self.path == '/get_messages':
             self.get_messages()
+
         elif self.path == '/login':
             data = json.loads(post_data)
             self.login(data['username'], data['password'])
@@ -108,17 +116,10 @@ class CustomRequestHandler(RangeHTTPServer.RangeRequestHandler):
         self.db.chat_conn.commit()
 
     def get_messages(self):
-        try:
-            with sqlite3.connect('chatroom-database.db') as conn:
-                cursor = conn.cursor()
-                cursor.execute('SELECT * FROM messages ORDER BY timestamp DESC LIMIT 50')
-                messages = cursor.fetchall()[::-1]
-                # Process the messages
-                messages = [{'username': x[1], 'message': x[2], 'timestamp': x[3]} for x in messages]
-                self.send_json_response(200, {'messages': messages})
-        except sqlite3.ProgrammingError as e:
-            print(f"SQLite error: {e}")
-            self.send_json_response(500, {'error': 'Internal Server Error'})
+        self.db.chat_cursor.execute('SELECT * FROM messages ORDER BY timestamp DESC LIMIT 50')
+        messages = self.db.chat_cursor.fetchall()[::-1]
+        messages = [{'username': x[1], 'message': x[2], 'timestamp': x[3]} for x in messages]
+        self.send_json_response(200, {'messages': messages})
 
     def login(self, username, password):
         logger.info("Logging in as " + username)
@@ -136,13 +137,13 @@ class CustomRequestHandler(RangeHTTPServer.RangeRequestHandler):
                 sessions[str(sessionID)] = ConnectedClient(username, sessionID)
                 self.send_json_response(200, {'success': True, "session": sessionID})
                 return 
-        else:
+        else: # new account creation
             self.db.login_cursor.execute('INSERT INTO login (username, password) VALUES (?, ?)', (username, password))
             self.db.login_conn.commit()
             # create a new row in the player_data table
             self.db.player_data_cursor.execute('INSERT INTO basicPlayerData (username) VALUES (?)', (username,))
             self.db.player_data_conn.commit()
-            random.seed(time.time())
+            # create a new session
             sessionID = hashlib.sha256(str(time.time()).encode('utf-8')).hexdigest()
             sessions[str(sessionID)] = ConnectedClient(username, sessionID)
             logger.info("Logged in as " + username + " with session " + sessionID + "sessions: " + str(sessions))
@@ -154,33 +155,31 @@ class CustomRequestHandler(RangeHTTPServer.RangeRequestHandler):
         self.end_headers()
         self.wfile.write(json.dumps(data).encode('utf-8'))
 
-# WebSocket connection handler
-connected_clients = []
 
 def start_inactivity_check(timeout):
     db = DatabaseManager()
     while True:
         for _ in range(100):
-            print("Checking inactive sessions...")
+            # every minute check for inactive sessions
             logger.info("Checking inactive sessions...")
             logger.info(sessions)
             current_time = time.time()
             inactive_sessions = [session_id for session_id, client in sessions.items() if current_time - client.lastActiveTime > timeout]
             
             for session_id in inactive_sessions:
-                print(f"Session {session_id} is inactive for too long. Removing user.")
                 logger.info(f"Session {session_id} is inactive for too long. Removing user.")
-                # del sessions[session_id]
+                del sessions[session_id]
             
             time.sleep(60)
 
+        # only once an hour check and delete guest accounts
         active_sessions = tuple(map(lambda x: x.username, list(sessions.values())))
         placeholders = ', '.join('?' for _ in active_sessions)
 
-        # db.login_cursor.execute(f'DELETE FROM login WHERE username LIKE "Guest%" AND username NOT IN ({placeholders})', active_sessions)
-        # db.login_conn.commit()
-        # db.player_data_cursor.execute(f'DELETE FROM basicPlayerData WHERE username LIKE "Guest%" AND username NOT IN ({placeholders})', active_sessions)
-        # db.player_data_conn.commit()
+        db.login_cursor.execute(f'DELETE FROM login WHERE username LIKE "Guest%" AND username NOT IN ({placeholders})', active_sessions)
+        db.login_conn.commit()
+        db.player_data_cursor.execute(f'DELETE FROM basicPlayerData WHERE username LIKE "Guest%" AND username NOT IN ({placeholders})', active_sessions)
+        db.player_data_conn.commit()
         
 
 with ThreadedHTTPServer(("", HTTP_PORT), CustomRequestHandler) as httpd:
